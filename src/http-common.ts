92622f9b2706d3575a63e7899dab8c05658c81eb
gyanu-kharel <kharelgyanoo@gmail.com>
2023-12-06 17:19:23 -0500

    base url changed

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://localhost:7161/api",
+  baseURL: "http://localhost:5133/api",
   headers: {
     "Content-type": "application/json"
   }
