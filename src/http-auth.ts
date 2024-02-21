fb78ec41807d64b18d08fd4edf6ac0ccdca97ceb
gyanu-kharel <kharelgyanoo@gmail.com>
2024-02-20 23:17:54 -0500

    fixes

--- src/http-auth.ts
+++ src/http-auth.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const appUser = localStorage.getItem('appUser');
 const parsed = JSON.parse(appUser!);
-const token = parsed.token;
+const token = parsed && parsed.token;
 
 export default axios.create({
     baseURL: "http://localhost:5250/api",
